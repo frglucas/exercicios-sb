831f43a9a1c21668bec3a3f6bbc8f935936b5009
Lucas Fraga <lucas291201@gmail.com>
2022-02-09 19:27:52 -0300

    buscar por Id

--- src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
+++ src/main/java/br/com/cod3r/exerciciossb/controllers/ProdutoController.java
@@ -1,9 +1,14 @@
 package br.com.cod3r.exerciciossb.controllers;
 
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,12 +23,29 @@ public class ProdutoController {
 	private ProdutoRepository produtoRepository;
 	
 	@PostMapping
-	public @ResponseBody Produto novoProduto(
-			@RequestParam String nome,
-			@RequestParam double preco, 
-			@RequestParam double desconto) {
-		Produto produto = new Produto(nome, preco, desconto);
+	public @ResponseBody Produto novoProduto(@Valid Produto produto) {
 		produtoRepository.save(produto);
 		return produto;
 	}
+	
+	@GetMapping
+	public Iterable<Produto> obterProdutos() {
+		return produtoRepository.findAll();
+	}
+	
+	@GetMapping(path = "/{id}")
+	public Optional<Produto> obterProdutoPorId(@PathVariable int id) {
+		return produtoRepository.findById(id);
+	}
+	
+//	@PostMapping
+//	public @ResponseBody Produto novoProduto(
+//			@RequestParam String nome,
+//			@RequestParam double preco, 
+//			@RequestParam double desconto) {
+//		Produto produto = new Produto(nome, preco, desconto);
+//		produtoRepository.save(produto);
+//		return produto;
+//	}
+	
 }
